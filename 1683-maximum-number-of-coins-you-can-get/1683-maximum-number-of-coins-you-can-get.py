6e057c72d150969283082fd9f7d68dad10ffe01f
Ubaida Abdul <128925635+MyBaida@users.noreply.github.com>
2025-06-17 21:36:26 +0100

    Time: 75 ms (24.53%), Space: 27.5 MB (64.84%) - LeetHub

--- 1683-maximum-number-of-coins-you-can-get/1683-maximum-number-of-coins-you-can-get.py
+++ 1683-maximum-number-of-coins-you-can-get/1683-maximum-number-of-coins-you-can-get.py
@@ -2,11 +2,11 @@ class Solution:
     def maxCoins(self, piles: List[int]) -> int:
         piles.sort(reverse = True)
         mine = 0
-
         l = 0
         r = len(piles)-1
 
         while l < r:
+
             l += 1
             mine += piles[l]
 
