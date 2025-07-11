269ac7dda61bdec8746f4bd99c3c7b4807cca64a
Ubaida Abdul <128925635+MyBaida@users.noreply.github.com>
2025-07-12 00:03:01 +0100

    Time: 3 ms (30.61%), Space: 17.8 MB (94.5%) - LeetHub

--- 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.py
@@ -1,8 +1,8 @@
 class Solution:
     def applyOperations(self, nums: List[int]) -> List[int]:
+
         for i in range(len(nums)-1):
             if nums[i] == nums[i+1]:
-
                 nums[i] *= 2
                 nums[i+1] = 0
 
@@ -10,8 +10,8 @@ class Solution:
         
         for j in range(len(nums)):
             if nums[j] != 0:
-
                 nums[i], nums[j] = nums[j], nums[i]
+                
                 i += 1
 
         return nums
